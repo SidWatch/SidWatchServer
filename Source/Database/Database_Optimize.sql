603f23c24121922ffcd2f62021587d8097d7b74a
Brian Nelson <brian@treegecko.com>
2014-09-19 10:06:47 -0600

    script to optimize db.

---
+++ Source/Database/Database_Optimize.sql
@@ -0,0 +1,11 @@
+use SidWatchData;
+
+optimize table accesskeys;
+optimize table files;
+optimize table migrations;
+optimize table sites;
+optimize table sitespectrumdata;
+optimize table sitespectrums;
+optimize table statiomreadings;
+optimize table statioms;
+optimize table users; 
